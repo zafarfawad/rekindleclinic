0870ff91b58f17dff456385e6cca23e9c1122430
Fawad Zafar <fawad.zafar@robotsandpencils.com>
2019-04-20 12:07:22 -0400

    everything

--- js/script.js
+++ js/script.js
@@ -26,6 +26,27 @@
             $("#mainNav").removeClass("navbar-shrink")
         }
     };
+
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+
+    $('.carousel').carousel({
+        interval: 2000
+    })
+    
     navbarCollapse();
     $(window).scroll(navbarCollapse)
 })(jQuery)
